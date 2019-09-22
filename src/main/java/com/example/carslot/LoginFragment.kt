884e0e676507367c7675351c10255054a67ed614
nattatida59160558 <59160558@go.buu.ac.th>
2019-09-22 23:49:48 +0700

    info2

--- src/main/java/com/example/carslot/LoginFragment.kt
+++ src/main/java/com/example/carslot/LoginFragment.kt
@@ -2,12 +2,11 @@ package com.example.carslot
 
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.carslot.databinding.FragmentLoginBinding
 import kotlinx.android.synthetic.main.fragment_login.*
 
@@ -25,6 +24,7 @@ class LoginFragment : Fragment() {
     private var username:String=""
     private var password:String=""
     private var boo:Boolean=false
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -68,7 +68,19 @@ class LoginFragment : Fragment() {
             }
 
         }
+        setHasOptionsMenu(true)
         return binding.root
+
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.options_menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!,view!!.findNavController())||super.onOptionsItemSelected(item)
     }
 
 
--- src/main/java/com/example/carslot/MainActivity.kt
+++ src/main/java/com/example/carslot/MainActivity.kt
@@ -7,6 +7,8 @@ import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.carslot.databinding.ActivityMainBinding
 
 
@@ -15,6 +17,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
+
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 
 
